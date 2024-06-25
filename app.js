f2798b94df36acf226312a0fc21ac6b8920c2f85
Iryna Siddiqui <irsidd11@gmail.com>
2024-06-25 16:26:53 -0400

    added git status!

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ function randomRGB() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`
+    const s = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b},${s})`
 }
 
 
